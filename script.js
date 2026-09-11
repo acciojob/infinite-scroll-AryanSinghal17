ee98aa8f5e150315ca6d0cd41c9413e53e33ad53
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-12 01:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.innerText = `Item ${count++}`;
+    list.appendChild(li);
+}
+
+
+window.addEventListener("scroll", () => {
+    if (
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight
+    ) {
+        for (let i = 0; i < 2; i++) {
+            const li = document.createElement("li");
+            li.innerText = `Item ${count++}`;
+            list.appendChild(li);
+        }
+    }
+});
