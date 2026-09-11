ce1d38861b63ee887dcccac9c104989eeaa85742
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-12 01:26:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let count = 1;
 
 for (let i = 0; i < 10; i++) {
     const li = document.createElement("li");
-    li.innerText = count++;
+    li.innerText = `Item ${count++}`;
     list.appendChild(li);
 }
 
 function addItems() {
     for (let i = 0; i < 2; i++) {
         const li = document.createElement("li");
-        li.innerText = count++;
+        li.innerText = `Item ${count++}`;
         list.appendChild(li);
     }
 }
@@ -19,7 +19,7 @@ function addItems() {
 window.addEventListener("scroll", () => {
     if (
         window.innerHeight + window.scrollY >=
-        document.body.scrollHeight
+        document.body.offsetHeight
     ) {
         addItems();
     }
